06d02da55e0395510b313e97d6e722580936acd4
Gennady Zykov <zykovgennady@gmail.com>
2021-10-18 21:59:01 +0300

    Update NormalizeAndScaleTest

--- tests/csharp/NN-Experiments.Common.Tests/Basic.cs
+++ tests/csharp/NN-Experiments.Common.Tests/Basic.cs
@@ -1,10 +1,26 @@
 using NNExperiments.Common;
 using NUnit.Framework;
+using System.Linq;
 
 namespace NNExperiments.Common.Tests
 {
     public class Basic
     {
+        private readonly double[][] _testData1 = new double[][]
+        {
+            new double[] { 0, 1, 2 },
+            new double[] { 0, 0.5, 1 },
+            new double[] { -1, 0, 1 }
+        };
+
+        private readonly double[][] _scaleRanges = new double[][]
+        {
+            new double[] { 0, 1 },
+            new double[] { -1, 1 },
+            new double[] { -1, 0 },
+            new double[] { -100, 100 }
+        };
+
         [SetUp]
         public void Setup()
         {
@@ -13,44 +29,49 @@ namespace NNExperiments.Common.Tests
         [Test]
         public void NormalizeAndScaleTest()
         {
-            double[][] data = new double[][]
-            {
-                new double[] { 0, 1, 2 },
-                new double[] { 0, 0.5, 1 },
-                new double[] { -1, 0, 1 }
-            };
-
-            double[][] scaledData0 = new double[3][];
-            double[][] scaledData1 = new double[3][];
-            double[][] scaledData2 = new double[3][];
-            double[][] scaledData3 = new double[3][];
-            double[][] scaledData4 = new double[3][];
+            double[][][] scaledData = new double[3][][];
 
             for (int i = 0; i < 3; i++)
             {
-                scaledData0[i] = CommonFunctions.Normalize(data[i]);
-                scaledData1[i] = CommonFunctions.Scale(data[i], 0, 1);
-                scaledData2[i] = CommonFunctions.Scale(data[i], -1, 1);
-                scaledData3[i] = CommonFunctions.Scale(data[i], -1, 0);
-                scaledData4[i] = CommonFunctions.Scale(data[i], -100, 100);
+                double[] normalizedData = CommonFunctions.Normalize(_testData1[i]);
+                Assert.AreEqual(normalizedData.Min(), 0);
+                Assert.AreEqual(normalizedData.Max(), 1);
+
+                scaledData[i] = NormalizeAndScaleSubTest1(_testData1[i]);
             }
 
-            double[][] scaledData5 = new double[3][];
-            double[][] scaledData6 = new double[3][];
-            double[][] scaledData7 = new double[3][];
-            double[][] scaledData8 = new double[3][];
-            double[][] scaledData9 = new double[3][];
-
+            scaledData = scaledData.Select(x => x.Reverse().ToArray()).ToArray();
             for (int i = 0; i < 3; i++)
             {
-                scaledData5[i] = CommonFunctions.Normalize(scaledData4[i]);
-                scaledData6[i] = CommonFunctions.Scale(scaledData3[i], 0, 1);
-                scaledData7[i] = CommonFunctions.Scale(scaledData2[i], -1, 1);
-                scaledData8[i] = CommonFunctions.Scale(scaledData1[i], -1, 0);
-                scaledData9[i] = CommonFunctions.Scale(scaledData0[i], -100, 100);
+                for (int k = 0; k < _scaleRanges.Length; k++)
+                {
+                    ScaleDataSubTest1(scaledData[i][k], _scaleRanges[i]);
+                }
             }
 
             Assert.Pass();
+        }
+
+        private double[][] NormalizeAndScaleSubTest1(double[] data)
+        {
+            double[][] result = new double[_scaleRanges.Length][];
+
+            for (int i = 0; i < _scaleRanges.Length; i++)
+            {
+                result[i] = ScaleDataSubTest1(data, _scaleRanges[i]);
+            }
+
+            return result;
+        }
+
+        private double[] ScaleDataSubTest1(double[] data, double[] scaleRange)
+        {
+            double[] scaledData = CommonFunctions.Scale(data, scaleRange[0], scaleRange[1]);
+            Assert.AreEqual(scaledData.Min(), scaleRange[0]);
+            Assert.AreEqual(scaledData.Max(), scaleRange[1]);
+            Assert.AreEqual(scaledData[1], (scaleRange[0] + scaleRange[1]) / 2);
+
+            return scaledData;
         }
     }
 }
